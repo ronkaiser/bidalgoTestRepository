6ce048fe7e72939b7a428171c198a5dad9c16724
ronkaiser <70015659+ronkaiser@users.noreply.github.com>
2020-12-25 00:44:11 +0200

    update commit logger script

--- commitLogger.py
+++ commitLogger.py
@@ -1,1 +1,21 @@
-print("add commit to DB")
+import mysql.connector
+import os
+
+COMMIT = os.getenv('GIT_COMMIT')
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="bidalgo",
+  database="mysql"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO commits (commit_id, commit_time, commit_message, commit_user) VALUES (%s, %s, %s, %s)"
+val = (COMMIT, "2", "3", "4")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
