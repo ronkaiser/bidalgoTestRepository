9e5668bef84e44b28baa455665e8291364f5092e
ronkaiser <70015659+ronkaiser@users.noreply.github.com>
2020-12-25 19:43:41 +0200

    add column to createtable file

--- files/createtable.py
+++ files/createtable.py
@@ -9,4 +9,4 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE commits (commit_id VARCHAR(255), commit_time VARCHAR(255), commit_message VARCHAR(255), commit_user VARCHAR(255))")
+mycursor.execute("CREATE TABLE commits (row_id int NOT NULL AUTO_INCREMENT, commit_id VARCHAR(255), commit_time VARCHAR(255), commit_message VARCHAR(255), commit_user VARCHAR(255))")
