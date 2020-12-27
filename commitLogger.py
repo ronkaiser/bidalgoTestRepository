d81e5a253e834b1a71cff22243c71b6f5cd4c73a
ronkaiser <70015659+ronkaiser@users.noreply.github.com>
2020-12-27 13:51:12 +0200

    print time

--- commitLogger.py
+++ commitLogger.py
@@ -3,10 +3,8 @@ import os
 import subprocess
 
 COMMIT = os.getenv('GIT_COMMIT')
-# USER = os.system('git log -1 | grep Author | cut -d " " -f2')
+TIME = subprocess.check_output('git log -1 | grep Date | cut -c 6-32', shell=True)
 USER = subprocess.check_output('git log -1 | grep Author | cut -d " " -f2', shell=True)
-
-print(USER)
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -26,7 +24,7 @@ mycursor.execute("""CREATE TABLE IF NOT EXISTS commits (
 			PRIMARY KEY(row_id))""")
 
 sql = "INSERT INTO commits (commit_id, commit_time, commit_message, commit_user) VALUES (%s, %s, %s, %s)"
-val = (COMMIT, "2", "3", USER)
+val = (COMMIT, TIME, "3", USER)
 mycursor.execute(sql, val)
 
 mydb.commit()
