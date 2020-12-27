847630a40c34b8b734c12c611423d89ade1e1bc2
ronkaiser <70015659+ronkaiser@users.noreply.github.com>
2020-12-27 12:53:53 +0200

    print user

--- commitLogger.py
+++ commitLogger.py
@@ -2,6 +2,7 @@ import mysql.connector
 import os
 
 COMMIT = os.getenv('GIT_COMMIT')
+USER = os.system('git log -1 | grep Author | cut -d " " -f2')
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -21,7 +22,7 @@ mycursor.execute("""CREATE TABLE IF NOT EXISTS commits (
 			PRIMARY KEY(row_id))""")
 
 sql = "INSERT INTO commits (commit_id, commit_time, commit_message, commit_user) VALUES (%s, %s, %s, %s)"
-val = (COMMIT, "2", "3", "4")
+val = (COMMIT, "2", "3", USER)
 mycursor.execute(sql, val)
 
 mydb.commit()
